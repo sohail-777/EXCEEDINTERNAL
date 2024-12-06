889fd12992596bd21a6b91cfdfc345c1c9ac7995
shaik sohail ahmmad <shaiksohailahmmad777@gmail.com>
2024-12-06 23:49:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EXCEEDINTERNAL
+ 
---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Login Successful!");
+    window.location.href = "main.html";
+});
